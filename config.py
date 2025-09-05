67f15fb26e8229e896ac2d80f076897331203db2
UHD Official <uhdnetwork@gmail.com>
2025-09-05 14:43:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,36 @@
 import os
-from typing import List
 
-API_ID = os.environ.get("API_ID", "")
+# Basic Bot Configs
+API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-PICS = (os.environ.get("PICS", "")).split()
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
+# Admin & Logging
+try:
+    ADMIN = int(os.environ.get("ADMIN", "0"))
+except ValueError:
+    ADMIN = None
 
+try:
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+except ValueError:
+    LOG_CHANNEL = None
+
+# Bot Images
+PICS = [x for x in os.environ.get("PICS", "").split() if x]
+
+# DB Settings
 DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "")
+DB_NAME = os.environ.get("DB_NAME", "AutoApprove")
 
-IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
+# Modes
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"
+IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"
+
+# Authorized Channels
+try:
+    AUTH_CHANNELS = list(
+        map(int, os.environ.get("AUTH_CHANNEL", "").split())
+    )
+except ValueError:
+    AUTH_CHANNELS = []
